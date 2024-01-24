cc0d2ef0e4816c13a10f011e496109e145f4dec5
ozziexyz <ozzieh@skiff.com>
2024-01-24 16:53:24 -0500

    "good nuff" Pivot code

---
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems.pivot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pivot extends SubsystemBase {
+    private final PivotIO io;
+    private final PivotIOInputsAutoLogged inputs = new PivotIOInputsAutoLogged();
+    private final ProfiledPIDController controller = 
+        new ProfiledPIDController(
+            PivotConstants.kP,
+            PivotConstants.kI,
+            PivotConstants.kD,
+            new TrapezoidProfile.Constraints(
+                PivotConstants.maxV, 
+                PivotConstants.maxA
+            )
+        );
+
+    private double targetAngle = 0.0;
+    private static Pivot instance;
+
+    public static Pivot getInstance(PivotIO io){
+        if(instance == null){
+            instance = new Pivot(io);
+        }
+        return instance;
+    }
+
+    public Pivot(PivotIO io) {
+        this.io = io;
+        io.updateInputs(inputs);
+    }
+
+    public void setTargetAngle(double targetAngle) {
+        this.targetAngle = MathUtil.clamp(targetAngle, PivotConstants.LowSetpointLimit, PivotConstants.HighSetpointLimit);
+    }
+
+    public double getPosition() { 
+        return inputs.angleRev;
+    }
+
+    public double getTargetAngle() {
+        return targetAngle;
+    }
+
+    @Override
+    public void periodic(){
+        io.setPivotVoltage(
+            controller.calculate(inputs.angleRev, 0)
+        );
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.pivot;
+
+// TODO: edit all of these
+public class PivotConstants {
+    public static final double LowSetpointLimit = 0;
+    public static final double HighSetpointLimit = 0;
+    public static final int CANID = 11;
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double maxV = 0;
+    public static final double maxA = 0;
+}

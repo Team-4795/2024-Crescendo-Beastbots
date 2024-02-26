2cd8134b27ed31540e5baa7aca1023f5f21c0690
ozziexyz <ozzieh@skiff.com>
2024-02-26 11:02:16 -0500

    fixed duplicate override from merge

--- src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
@@ -22,11 +22,6 @@ public class PivotIOSim implements PivotIO {
         );
     }
 
-    public void setPivotVoltage(double volts) {
-        appliedVolts = volts;
-        pivotSim.setInputVoltage(appliedVolts);
-    }
-
     @Override
     public void setPivotVoltage(double volts) {
         pivotSim.setInput(volts);

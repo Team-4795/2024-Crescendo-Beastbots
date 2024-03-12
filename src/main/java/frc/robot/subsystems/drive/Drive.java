65ff53fb19e2cba77e17b316a2c059f3df5582c2
ozziexyz <ozzieh@skiff.com>
2024-03-12 16:30:57 -0400

    updated vision stuff

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -21,17 +21,13 @@ import com.pathplanner.lib.pathfinding.Pathfinding;
 import com.pathplanner.lib.util.PathPlannerLogging;
 import com.pathplanner.lib.util.ReplanningConfig;
 
-import Jama.Matrix;
-import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Time;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -39,12 +35,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.Mode;
 import frc.robot.subsystems.vision.Vision;
-import frc.robot.util.LimelightHelpers;
 import frc.robot.util.LocalADStarAK;
-import frc.robot.util.LimelightHelpers.LimelightResults;
-
-import org.littletonrobotics.junction.AutoLogOutput;
-import org.littletonrobotics.junction.Logger;
 
 public class Drive extends SubsystemBase {
   public static final double WHEEL_RADIUS = Units.inchesToMeters(3.0);
--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -3,12 +3,9 @@ package frc.robot.subsystems.vision;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.drive.Drive;
 import frc.robot.util.LimelightHelpers;
-import frc.robot.util.LimelightHelpers.PoseEstimate;
 
 public class Vision extends SubsystemBase {
     private VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
@@ -38,8 +35,12 @@ public class Vision extends SubsystemBase {
         return inputs.area;
     }
 
-    public PoseEstimate getVisionpose(){
-        return inputs.limelightMeasurement;
+    public Pose2d getVisionPose(){
+        return inputs.limelightMeasurement.pose;
+    }
+
+    public double getVisionTimestamp() {
+        return inputs.limelightMeasurement.timestampSeconds;
     }
 
     @Override
